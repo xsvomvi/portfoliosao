f10bcf06edd909512d2aa93cdb1f7068a137518f
Saomai <s.ngo@student.fontys.nl>
2025-10-30 13:51:55 +0100

    + created basic navbar

---
+++ app/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+            <li><a href="#top">Home</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#skills">Skills</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
+'use client'
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
-    <></>
+    <>
+    <Navbar />
+    </>
   );
 }
